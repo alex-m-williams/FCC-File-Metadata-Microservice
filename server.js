637e541337b31ff8b8d121f4703f6fb48bda31af
awilliams27 <alexwilliams567@gmail.com>
2017-12-12 21:31:23 +0000

    first commit, added file upload & size check

--- server.js
+++ server.js
@@ -44,23 +44,12 @@ app.route('/')
 		  res.sendFile(process.cwd() + '/views/index.html');
     })
 
-app.post('/', multer.single('upl'), function(req,res){
-	console.log(req.body); //form fields
-	/* example output:
-	{ title: 'abc' }
-	 */
-	console.log(req.file); //form files
-	/* example output:
-            { fieldname: 'upl',
-              originalname: 'grumpy.png',
-              encoding: '7bit',
-              mimetype: 'image/png',
-              destination: './uploads/',
-              filename: '436ec561793aa4dc475a88e84776b1b9',
-              path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
-              size: 277056 }
-	 */
-	res.status(204).end();
+app.post('/', upload.single('fileupload'), function(req,res){
+  let responseObj = {size: req.file.size};
+  
+  res.writeHead(200, {'Content-Type': 'application/json' });
+	res.write(JSON.stringify(responseObj));
+  res.end();
 });
 
 // Respond not found to all the wrong routes
